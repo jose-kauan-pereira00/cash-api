fe9ecf6b6b29b6cd49b53472609b69ee448828ed
Jose Kauan Pereira <jose.kauan.pereira00@gmail.com>
2026-05-22 20:17:00 -0300

    First Commit

---
+++ src/main/java/com/invict/cash_api/cash_api/CashApiApplication.java
@@ -0,0 +1,13 @@
+package com.invict.cash_api.cash_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invict/cash_api/cash_api/CashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invict.cash_api.cash_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
